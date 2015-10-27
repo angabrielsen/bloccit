07dcde7e0a13ac16a1ec3a53c5257afe5a1f9300
Ashton N. Levier <anlevier@gmail.com>
2015-10-26 22:53:53 -0600

    reinstall gitbash

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
+  resources :advertisements
   resources :posts
 
   get 'about' => 'welcome#about'
-
-  get 'welcome/contact'
-
-  get 'welcome/faq'
 
   root to: 'welcome#index'
 end
