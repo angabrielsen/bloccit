d958744e8550c2e9a62113a19ba268b1f9954789
Ashton N. Levier <anlevier@gmail.com>
2015-11-24 02:09:53 -0700

    pre rm index

--- app/controllers/sponsoredposts_controller.rb
+++ app/controllers/sponsoredposts_controller.rb
@@ -1,8 +1,4 @@
 class SponsoredpostsController < ApplicationController
-	def index
-		@sponsoredposts = Sponsoredpost.all
-	end
-
 	def show
 		@sponsoredpost = Sponsoredpost.find(params[:id])
 	end
@@ -23,9 +19,37 @@ class SponsoredpostsController < ApplicationController
 			flash[:error] = "Error creating sponsored post. Please try again."
 			render :new
 		end
+	end
 
-		def edit
-			@sponsoredpost = Sponsoredpost.find(params[:id])
+	def edit
+		@sponsoredpost = Sponsoredpost.find(params[:id])
+	end
+
+	def update
+		@sponsoredpost = Sponsoredpost.find(params[:id])
+
+		@sponsoredpost.title = params[:sponsoredpost][:title]
+		@sponsoredpost.body = params[:sponsoredpost][:body]
+		@sponsoredpost.price = params[:sponsoredpost][:price]
+
+		if @sponsoredpost.save
+			flash[:notice] = "Sponsored post was updated."
+			redirect_to @sponsoredpost
+		else
+			flash[:error] = "Error saving topic. Please try again."
+			render :edit
+		end
+	end
+
+	def destroy
+		@sponsoredpost = Sponsoredpost.find(params[:id])
+
+		if @sponsoredpost.destroy
+			flash[:notice] = "\"#{@sponsoredpost.title}\" was deleted successfully."
+			redirect_to action: :index
+		else
+			flash[:error] = "There was an error deleting the sponsoredpost."
+			render :show
 		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-	resources :sponsoredposts
 	resources :topics do
 		resources :posts, except: [:index]
+		resources :sponsoredposts, except: [:index]
 	end
 
   get 'about' => 'welcome#about'
-
-  get 'welcome/contact'
-
-  get 'welcome/faq'
-
   root to: 'welcome#index'
 end
