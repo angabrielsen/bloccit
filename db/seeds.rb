32ecd35ace3231368f9859e820dfa68817c5916b
Ashton Levier <anlevier@gmail.com>
2015-12-22 00:20:58 -0700

    checkpoint 40 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,9 +45,16 @@ posts = Post.all
    )
 end
 
-user = User.first
-user.update_attributes!(
-	email: 'anlevier@gmail.com',
+admin = User.create!(
+	name: 'Admin User',
+	email: 'admin@example.com',
+	password: 'hellowrold',
+	role: 'admin'
+)
+
+member = User.create!(
+	name: 'Member User',
+	email: 'member@example.com',
 	password: 'helloworld'
 )
 
