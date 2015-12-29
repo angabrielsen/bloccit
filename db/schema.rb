f19f69a4fa12f41f284d71a971daa02e8752c07a
Ashton Levier <anlevier@gmail.com>
2015-12-29 14:27:54 -0700

    pause for meeting at votes_controller_spec.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227183007) do
+ActiveRecord::Schema.define(version: 20151229192013) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -57,8 +57,10 @@ ActiveRecord::Schema.define(version: 20151227183007) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.integer  "rating_id"
   end
 
+  add_index "posts", ["rating_id"], name: "index_posts_on_rating_id"
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
@@ -66,6 +68,12 @@ ActiveRecord::Schema.define(version: 20151227183007) do
     t.string   "title"
     t.text     "body"
     t.boolean  "resolved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "severity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -87,6 +95,7 @@ ActiveRecord::Schema.define(version: 20151227183007) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "rating_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -97,5 +106,16 @@ ActiveRecord::Schema.define(version: 20151227183007) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
---
+++ spec/controllers/votes_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+include RandomData
+include SessionsHelper
+
+RSpec.describe VotesController, type: :controller do
+
+end
