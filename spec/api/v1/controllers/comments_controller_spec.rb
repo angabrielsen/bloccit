f0ca823b3844bb25a7395ea98f960b7456b479b7
Ashton Levier <anlevier@gmail.com>
2016-01-22 01:28:34 -0700

    assignment 48 complete

--- spec/api/v1/controllers/comments_controller_spec.rb
+++ spec/api/v1/controllers/comments_controller_spec.rb
@@ -1,15 +1,28 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe Api::V1::CommentsController, type: :controller do
   let(:my_user) { create(:user) }
   let(:my_topic) { create(:topic) }
-  let(:my_post) { create(:post) }
+  let(:my_post) { create(:post, topic: my_topic, user: my_user) }
   let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, user: my_user) }
 
   context "unauthenticated user" do
+    it "GET index return http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
     it "GET show returns http success" do
       get :show, id: my_comment.id
       expect(response).to have_http_status(:success)
+    end
+
+    it "GET show returns a comment" do
+      get :show, id: my_comment.id
+      response_hash = JSON.parse response.body
+      expect(response_hash['id']).to eq my_comment.id
+      expect(response_hash['body']).to eq my_comment.body
     end
   end
 
@@ -18,9 +31,21 @@ RSpec.describe Api::V1::CommentsController, type: :controller do
       controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
     end
 
+    it "GET index return http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
     it "GET show returns http success" do
       get :show, id: my_comment.id
       expect(response).to have_http_status(:success)
+    end
+
+    it "GET show returns a comment" do
+      get :show, id: my_comment.id
+      response_hash = JSON.parse response.body
+      expect(response_hash['id']).to eq my_comment.id
+      expect(response_hash['body']).to eq my_comment.body
     end
   end
 end
